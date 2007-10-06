4363ff44c2f8f89d6c38daa940a309890402ce57
yuri <yuri@70e4c66e-1a1a-0410-b208-ef94897248b2>
2007-10-06 20:00:00 +0000

    fixed colour selection

--- kolja/curses-list/src/main/java/com/baulsupp/curses/list/ColorList.java
+++ kolja/curses-list/src/main/java/com/baulsupp/curses/list/ColorList.java
@@ -1,6 +1,21 @@
+/**
+ * Copyright (c) 2002-2007 Yuri Schimke. All Rights Reserved.
+ * 
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ * 
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this library; if not, write to the Free Software Foundation, Inc.,
+ * 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+ */
 package com.baulsupp.curses.list;
-
-import java.awt.Color;
 
 import jcurses.system.CharColor;
 
@@ -11,13 +26,12 @@ import com.baulsupp.kolja.util.colours.ColourPair;
 
 public class ColorList {
   public static final Logger logger = Logger.getLogger(ColorList.class);
-  
+
   public static final CharColor blackOnWhite = new CharColor(CharColor.WHITE, CharColor.BLACK);
 
   public static final CharColor blueOnWhite = new CharColor(CharColor.WHITE, CharColor.BLUE);
 
-  public static final CharColor redOnWhite = new CharColor(CharColor.WHITE, CharColor.RED, CharColor.NORMAL,
-      CharColor.BOLD);
+  public static final CharColor redOnWhite = new CharColor(CharColor.WHITE, CharColor.RED, CharColor.NORMAL, CharColor.BOLD);
 
   public static final CharColor yellowOnWhite = new CharColor(CharColor.WHITE, CharColor.YELLOW);
 
@@ -31,8 +45,7 @@ public class ColorList {
 
   public static final CharColor blueOnBlack = new CharColor(CharColor.BLACK, CharColor.BLUE);
 
-  public static final CharColor redOnBlack = new CharColor(CharColor.BLACK, CharColor.RED, CharColor.NORMAL,
-      CharColor.BOLD);
+  public static final CharColor redOnBlack = new CharColor(CharColor.BLACK, CharColor.RED, CharColor.NORMAL, CharColor.BOLD);
 
   public static final CharColor yellowOnBlack = new CharColor(CharColor.BLACK, CharColor.YELLOW);
 
@@ -46,30 +59,30 @@ public class ColorList {
     if (colorPair == null) {
       return ColorList.whiteOnBlack;
     }
-    
-    short foreground = getColor(colorPair.getForeground(), CharColor.WHITE);
-    short background = getColor(colorPair.getBackground(), CharColor.BLACK);
-    
+
+    short foreground = getColour(colorPair.getForeground(), CharColor.WHITE);
+    short background = getColour(colorPair.getBackground(), CharColor.BLACK);
+
     CharColor c = new CharColor(background, foreground);
     return c;
   }
 
-  private static short getColor(Colour foreground, short defaultColor) {
-    if (foreground.equals(Color.BLACK)) {
+  private static short getColour(Colour foreground, short defaultColor) {
+    if (foreground.equals(Colour.BLACK)) {
       return CharColor.BLACK;
-    } else if (foreground.equals(Color.BLUE)) {
+    } else if (foreground.equals(Colour.BLUE)) {
       return CharColor.BLUE;
-    } else if (foreground.equals(Color.CYAN)) {
+    } else if (foreground.equals(Colour.CYAN)) {
       return CharColor.CYAN;
-    } else if (foreground.equals(Color.GREEN)) {
+    } else if (foreground.equals(Colour.GREEN)) {
       return CharColor.GREEN;
-    } else if (foreground.equals(Color.MAGENTA)) {
+    } else if (foreground.equals(Colour.MAGENTA)) {
       return CharColor.MAGENTA;
-    } else if (foreground.equals(Color.RED)) {
+    } else if (foreground.equals(Colour.RED)) {
       return CharColor.RED;
-    } else if (foreground.equals(Color.WHITE)) {
+    } else if (foreground.equals(Colour.WHITE)) {
       return CharColor.WHITE;
-    } else if (foreground.equals(Color.YELLOW)) {
+    } else if (foreground.equals(Colour.YELLOW)) {
       return CharColor.YELLOW;
     } else {
       return defaultColor;
--- kolja/kolja-viewer/src/test/java/com/baulsupp/kolja/log/viewer/request/RegexFieldCopierTest.java
+++ kolja/kolja-viewer/src/test/java/com/baulsupp/kolja/log/viewer/request/RegexFieldCopierTest.java
@@ -8,22 +8,22 @@ import com.baulsupp.kolja.log.line.Line;
 public class RegexFieldCopierTest extends TestCase {
   public void testCopiesFieldFromInputLine() {
     RegexFieldCopier copier = new RegexFieldCopier("content", "(aaa)", "content2");
-    
+
     Line line = new BasicLine("aaa");
     line.setValue("content", "baaab");
     RequestLine requestLine = new RequestLine("a", "ok");
     copier.copy(line, requestLine);
-    
+
     assertEquals("aaa", requestLine.getValue("content2"));
   }
-  
+
   public void testWorksWithNullField() {
     RegexFieldCopier copier = new RegexFieldCopier("content", "(.*)", "content2");
-    
+
     Line line = new BasicLine("aaa");
     RequestLine requestLine = new RequestLine("a", "ok");
     copier.copy(line, requestLine);
-    
+
     assertNull(requestLine.getValue("content2"));
   }
 }
